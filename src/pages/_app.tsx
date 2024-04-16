375bf175d62b5926c4bdcd564f0532010f3e4eb6
lsj0202 <w1004123@gmail.com>
2024-04-17 00:50:23 +0900

    feat: add footer 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex h-screen w-screen">
-      <div className="flex flex-col">
-        <Component {...pageProps} />
+      <div className="flex flex-1 flex-col">
+        <Header />
+        <div className="flex flex-1 flex-col overflow-y-auto">
+          <main className="bg-slate-50 flex-1">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </div>
       </div>
     </div>
   );
