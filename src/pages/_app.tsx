73fa8ddb28b431f682e7111db06d27bca25608f0
lsj0202 <w1004123@gmail.com>
2024-04-17 00:14:52 +0900

    feat: add supabase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,11 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex h-screen w-screen">
+      <div className="flex flex-col">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
